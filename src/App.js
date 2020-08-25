32802bff24d8be8dc33ec722a98b8849cf89a26e
Tayab Pabel <tayabpabel@gmail.com>
2020-08-25 16:46:20 +0600

    ultra-nation-done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(data => setCountries(data))
+    .catch(error => console.log(error))
   }, [])
 
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  }
 
   return (
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
+      <h4>Country Added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+        {
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+        }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
